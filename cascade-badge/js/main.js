eebcf9b7bc367fc0c377348443966eb284ad8b8d
damolabolaji <bolaji.oyindamola@gmail.com>
2023-09-03 13:12:24 +0100

    created filteredArray usinf filter method

--- cascade-badge/js/main.js
+++ cascade-badge/js/main.js
@@ -84,9 +84,16 @@ function checkGreater(arr1, arr2) {
 }
 
 console.log(checkGreater(myArray, myArray));
+
 //Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
 // Some cases:
 // [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
 // [68, -1, 1, -7, 10, 10] => [-1, 10]
+//if element % index ===0, return the element
+
+const theArray = [22, -6, 32, 82, 9, 25];
+
+let filtereArray = theArray.filter((item, index) => item % index === 0);
+console.log(filtereArray);
 
 //Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.Return your answer as a number.
