3e1c17ae228075f9932a897c4a7aaff242f1a092
damolabolaji <bolaji.oyindamola@gmail.com>
2023-09-03 12:32:32 +0100

    created getSquares function

--- cascade-badge/js/main.js
+++ cascade-badge/js/main.js
@@ -38,11 +38,10 @@ function randomShuffle(array) {
     //THIRD TIME AROUND j=2
     //store what is at array[3]-- yonsie
     //chamander,caterpie,pikachu,pikachu   //chamnadr,caterpie,yonsie,pikachu
-      
+
     //THIRD TIME AROUND j=3
-  //store array[0] --chamandr
-      //pikachu,caterpie,yonsie,pikchu // pikachu,caterpie,yomsi,chamader
-      
+    //store array[0] --chamandr
+    //pikachu,caterpie,yonsie,pikchu // pikachu,caterpie,yomsi,chamader
   }
 
   return array;
@@ -55,14 +54,24 @@ console.log(randomShuffle(arr));
 // }
 // console.log(printRandomRange(5, 15));
 
-
 //Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater
 //than the sum of the cubes of each element in b.
 //get the sum of the squares of a
 //get the sum of the cubes of b
-function getSquares(arr){
-  
+function getSquares(arr) {
+  return arr.map((item) => {
+    item ** 2;
+  });
 }
+
+console.log(getSquares([1, 2, 3]));
+const myArray = [1, 2, 3];
+
+function squaredIt(arr) {
+  return arr.map((item) => item ** 2);
+}
+
+console.log(squaredIt(myArray));
 //Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
 // Some cases:
 // [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
